e5d428731b9502dfb41c77f75aec6f35edd8c490
Dave Smith <dave.k.smith@gmail.com>
2019-02-17 13:38:00 -0600

    Put KotlinVersion.latest and update under test

--- tests/local/kotlin_version_test.py
+++ tests/local/kotlin_version_test.py
@@ -8,24 +8,26 @@ def test_KotlinVersion_existing(mocker):
     version = KotlinVersion.existing()
     assert version == '4'  # nosec
 
-#def test_KotlinVersion_latest(mocker):
-#    """unit test KotlinVersion.latest"""
-#    getmock = mocker.patch('devops_spt.kotlin_version.get')
-#    loadsmock = mocker.patch('devops_spt.kotlin_version.loads')
-#    loadsmock.return_value = {'version': '18'}
-#    version = KotlinVersion.latest()
-#    getmock.assert_called_once()
-#    loadsmock.assert_called_once()
-#    assert version == '18'  # nosec
-#
-#def test_KotlinVersion_update_not_needed(mocker):
-#    """unit test KotlinVersion.update, no update necessary"""
-#    gvmock = mocker.patch('devops_spt.kotlin_version.KotlinVersion')
-#    gvmock.existing.return_value = '6'
-#    gvmock.latest.return_value = '6'
-#    KotlinVersion.update(verbose=True)
-#    gvmock.existing.assert_called_once()
-#
+def test_KotlinVersion_latest(mocker):
+    """unit test KotlinVersion.latest"""
+    getmock = mocker.patch('devops_spt.kotlin_version.get')
+    urlmock = mocker.patch('devops_spt.kotlin_version.urlparse')
+    basemock = mocker.patch('devops_spt.kotlin_version.basename')
+    basemock.return_value ='v8'
+    version = KotlinVersion.latest()
+    getmock.assert_called_once()
+    urlmock.assert_called_once()
+    basemock.assert_called_once()
+    assert version == '8'  # nosec
+
+def test_KotlinVersion_update_not_needed(mocker):
+    """unit test KotlinVersion.update, no update necessary"""
+    gvmock = mocker.patch('devops_spt.kotlin_version.KotlinVersion')
+    gvmock.existing.return_value = '6'
+    gvmock.latest.return_value = '6'
+    KotlinVersion.update(verbose=True)
+    gvmock.existing.assert_called_once()
+
 #def test_KotlinVersion_update_needed(mocker):
 #    """unit test KotlinVersion.update, update necessary"""
 #    gvmock = mocker.patch('devops_spt.kotlin_version.KotlinVersion')
