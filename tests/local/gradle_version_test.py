d8698035bad511bc7df593d9fbb2d891088ad0de
Dave Smith <dave.k.smith@gmail.com>
2019-02-17 10:49:42 -0600

    Bring GradleVersion.latest under test

--- tests/local/gradle_version_test.py
+++ tests/local/gradle_version_test.py
@@ -3,9 +3,18 @@ from devops_spt import GradleVersion
 
 def test_GradleVersion_existing(mocker):
     """unit test GradleVersion.existing"""
-    # patch subprocess.run with desired side effect for testing
     runmock = mocker.patch('devops_spt.gradle_version.run')
     runmock.return_value.stdout = 'Gradle 0.9.8'
     version = GradleVersion.existing()
     runmock.assert_called_once()
     assert version == '0.9.8'  # nosec
+
+def test_GradleVersion_latest(mocker):
+    """unit test GradleVersion.latest"""
+    getmock = mocker.patch('devops_spt.gradle_version.get')
+    loadsmock = mocker.patch('devops_spt.gradle_version.loads')
+    loadsmock.return_value = {'version': '18'}
+    version = GradleVersion.latest()
+    getmock.assert_called_once()
+    loadsmock.assert_called_once()
+    assert version == '18'  # nosec
