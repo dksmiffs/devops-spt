a743e44428bb39327472b18c3fa4023226e62b8c
Dave Smith <dave.k.smith@gmail.com>
2019-02-23 11:20:26 -0600

    Reflect rename of cd.py module to directory.py

--- tests/local/cd_test.py
+++ tests/local/cd_test.py
@@ -1,32 +1,31 @@
-"""pytest unit tests for the cd module"""
-import os
-from devops_spt import cd
+"""pytest unit tests for the Directory module"""
+from devops_spt import Directory
 
-def test_chdir(mocker):
-    """unit test cd.chdir"""
-    mocker.patch('os.getcwd')
-    mocker.patch('os.chdir')
-    mocker.patch('os.path.expanduser')
+def test_cd(mocker):
+    """unit test Directory.cd"""
+    getcwd_mock = mocker.patch('devops_spt.directory.getcwd')
+    chdir_mock = mocker.patch('devops_spt.directory.chdir')
+    expand_mock = mocker.patch('devops_spt.directory.path.expanduser')
     # Note below that assert_called_once(), assert_called_once_with(), and
     #    resetall() are valid members on the mock, so we ignore pylint E1101
-    with cd.chdir('somedir'):
+    with Directory.cd('somedir'):
         # pylint:disable-msg=E1101
-        os.getcwd.assert_called_once()
-        os.path.expanduser.assert_called_once_with('somedir')
-        os.chdir.assert_called_once()
+        getcwd_mock.assert_called_once()
+        expand_mock.assert_called_once_with('somedir')
+        chdir_mock.assert_called_once()
         mocker.resetall()
     # chdir (back to prev) should've been called on context exit
-    os.chdir.assert_called_once()  # pylint:disable-msg=E1101
+    chdir_mock.assert_called_once()  # pylint:disable-msg=E1101
 
     # Same, but raise an exception during the chdir context
     mocker.resetall()
     try:
-        with cd.chdir('diffdir'):
+        with Directory.cd('diffdir'):
             # pylint:disable-msg=E1101
-            os.chdir.assert_called_once()
+            chdir_mock.assert_called_once()
             mocker.resetall()
-            raise RuntimeError('some exception during chdir context')
+            raise RuntimeError('some exception during cd context')
     except RuntimeError:
         pass
     # chdir (back to prev) should've also been called on exception
-    os.chdir.assert_called_once()  # pylint:disable-msg=E1101
+    chdir_mock.assert_called_once()  # pylint:disable-msg=E1101
